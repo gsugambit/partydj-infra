2234fc9707d9cb294fe28599d997893bdae00f7a
GSUGambit <gsugambit@gmail.com>
2022-04-22 23:26:57 -0700

    removing reference to http server

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -10,10 +10,3 @@ output "firewall_rule" {
   value = "${module.firewall.firewall_rule}"
 }
 
-output "instance_name" {
-  value = "${module.http_server.instance_name}"
-}
-
-output "external_ip" {
-  value = "${module.http_server.external_ip}"
-}
