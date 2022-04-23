7e28beeefa299159f0df929836e206ce9b0fab33
GSUGambit <gsugambit@gmail.com>
2022-04-22 23:37:50 -0700

    adding https

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -15,3 +15,18 @@ resource "google_compute_firewall" "allow-http" {
   target_tags   = ["allow-https"]
   source_ranges = ["0.0.0.0/0"]
 }
+
+resource "google_compute_firewall" "allow-https" {
+  name    = "${local.network}-allow-https"
+  network = "${local.network}"
+  project = "${var.project}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags   = ["allow-https"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
--- modules/firewall/outputs.tf
+++ modules/firewall/outputs.tf
@@ -1,3 +1,8 @@
 output "firewall_rule" {
   value = "${google_compute_firewall.allow-http.name}"
 }
+
+output "firewall_rule" {
+  value = "${google_compute_firewall.allow-https.name}"
+}
+
